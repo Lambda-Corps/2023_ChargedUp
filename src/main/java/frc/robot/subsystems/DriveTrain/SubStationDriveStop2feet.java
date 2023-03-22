4321ade7c3949f9df84da9ac910516baefda993b
1895github <98063256+1895github@users.noreply.github.com>
2023-03-22 16:58:19 -0400

    More Modified changes

--- src/main/java/frc/robot/subsystems/DriveTrain/SubStationDriveStop2feet.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/SubStationDriveStop2feet.java
@@ -5,28 +5,51 @@
 package frc.robot.subsystems.DriveTrain;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class SubStationDriveStop2feet extends CommandBase {
+  private final DriveTrain m_drivetrain;
+  private final CommandXboxController m_driver_controller;
   /** Creates a new SubStationDriveStop2feet. */
-  public SubStationDriveStop2feet() {
+  private boolean is_done;
+  private double drive_speed;
+  public SubStationDriveStop2feet(DriveTrain dt, CommandXboxController xbox) {
+    m_drivetrain = dt;
+    m_driver_controller = xbox;
+
+    drive_speed = 0.3; // 30% motor output
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double raw_turn_axis = m_driver_controller.getRawAxis(1);
+    is_done = m_drivetrain.DriveUntil2FeetFromSubStation(drive_speed, raw_turn_axis);
+    
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drivetrain.teleop_drive(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if (is_done == true){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
